9925837b7472f2fc129fac0a664aa98a82343f2c
RubenNavarro0 <1914479@alu.murciaeduca.es>
2026-05-04 22:13:19 +0200

    cambio?

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
         {
             policy.WithOrigins(
                 "http://localhost:5173",
-            "https://gymderubensindiegoyalex.netlify.app/"
+            "https://gymderubensindiegoyalex.netlify.app"
             )
                   .AllowAnyHeader()
                   .AllowAnyMethod();
